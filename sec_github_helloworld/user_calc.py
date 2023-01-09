d8c61087d8a9f2abde9975696f7f12b06429cdb0
anandhu <anandhudj2231@gmail.com>
2023-01-09 21:30:42 +0530

    new

--- sec_github_helloworld/user_calc.py
+++ sec_github_helloworld/user_calc.py
@@ -1,8 +1,5 @@
-
-
 a=input("Please type down a number: ")
 b=input("Please type down another number: ")
-c=str(a)+str(b)
-d = str(a)-str(b)
-printf(f"{e} is the addition of the two numbers")
+c=int(a)+int(b)
+print(f"{c} is the addition of the two numbers")
     
