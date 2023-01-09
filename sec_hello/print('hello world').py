78a4952923ea393c67a134dd852d08fa4f3f4e37
anandhu <anandhudj2231@gmail.com>
2023-01-09 14:18:08 +0530

    new add.py

---
+++ sec_hello/print('hello world').py
@@ -0,0 +1,24 @@
+print('hello world')
+
+
+##new change
+print("new change")
+print("new line")
+#haiii i'm akshitha
+#i'm adding a code for checking even or odd in your program
+num=int(input("enter a number")
+if(num%2)==0:
+        print("the number is even")
+else:
+        print("the number is odd")
+        
+        
+#even or odd using function
+def oddeven(number):
+        if(number%2==0):
+                return(f"{number} is even")
+        else:
+                return(f"{number} is odd")
+print(oddeven(20))
+
+
