f0cfbcc70c839b950c98d338744abadbb1adcad8
anandhu <anandhudj2231@gmail.com>
2023-01-09 14:44:01 +0530

    new222

--- sec_github_helloworld/hello.py
+++ sec_github_helloworld/hello.py
@@ -2,18 +2,9 @@ print('hello, trying github!!!')
 print('Malavika is in github with ssh key!!!!!!')
 print('added commit message!!!')
 
-print("my new edit")
-print("Learn New stuffs")
-
-print("latest update")
+def add(a,b):
+    print('addition')
+    return a+b
 
 def HiTeller():
     return f" Malu is awesome"
-
-
-a = int(input("Enter number1 : -"))
-b = int(input("enter number2 : -"))
-
-print(f"Sum is {a+b}")
-
-# print(HiTeller())
---
+++ sec_github_helloworld/user_calc.py
@@ -0,0 +1,8 @@
+
+
+a=input("Please type down a number: ")
+b=input("Please type down another number: ")
+c=str(a)+str(b)
+d = str(a)-str(b)
+printf(f"{e} is the addition of the two numbers")
+    
